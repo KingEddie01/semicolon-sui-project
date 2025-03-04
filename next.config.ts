39892766b096d330182d6c5f457969f62b4a2306
king-eddie <edk6004@gmail.com>
2025-03-04 13:36:39 +0100

    individual and team card reimplementation and favicon added

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ["suihackerhouse.semicolon.africa"], // Add the allowed image domain
+    },
   /* config options here */
 };
 
--- src/page-component /fifthComponent/index.tsx
+++ src/page-component /fifthComponent/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { GiCheckMark } from "react-icons/gi";
+import Image from "next/image"
 
 
 function FifthPage() {
@@ -9,8 +10,8 @@ function FifthPage() {
             <section className="mx-auto my-8 max-w-6xl px-4 py-8">
                 <div className="grid gap-8 md:gap-16 md:grid-cols-2">
                     <div className="relative h-[300px] w-full overflow-hidden rounded-xl">
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img
+
+                        <Image
                             src="https://suihackerhouse.semicolon.africa/participation-requirement-image.png"
                             alt="Workspace with laptop"
                             width={500}

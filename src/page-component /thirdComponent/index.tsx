d5777aa9e53f49dd7e9ce6431c8640a39d9d742e
king-eddie <edk6004@gmail.com>
2025-03-04 09:03:57 +0100

    semicolon sui web page

--- src/page-component /thirdComponent/index.tsx
+++ src/page-component /thirdComponent/index.tsx
@@ -5,7 +5,7 @@ const ThirdPage = () => {
         <div className="grid justify-center md:grid-cols-2 md:mb-16">
             <div className="bg-blue-100 py-16 px-8">
                 <h2 className="text-2xl font-bold text-blue-600 mb-6">Features of the Hacker House</h2>
-                <ul className="list-disc pl-5 space-y-3 text-black">
+                <ul className="list-disc pl-5 space-y-3 text-black marker:text-blue-600">
                     <li>Brainstorming and Ideation sessions</li>
                     <li>Workshops and Tutorials</li>
                     <li>Coaching and Mentorship from Move Experts, including the Sui Foundation Head of Developer Relations</li>
@@ -15,8 +15,8 @@ const ThirdPage = () => {
                 </ul>
             </div>
             <div className="bg-blue-50 py-16 px-8">
-                <h2 className="text-2xl font-bold text-blue-600 mb-6">Benefits for Participating Teams</h2>
-                <ul className="list-disc pl-5 space-y-3 text-black">
+                <h2 className="text-2xl font-bold text-blue-600 mb-6 ">Benefits for Participating Teams</h2>
+                <ul className="list-disc pl-5 space-y-3 text-black marker:text-blue-600">
                     <li>Access Mentorship & Coaching</li>
                     <li>Engage with Sui Foundation</li>
                     <li>Hands-On Learning</li>
